95f798cbea8f9f745a76b899c51b81fa59dbc055
jeeranat <jeeranat.sith@gmail.com>
2016-09-17 18:56:00 -0400

    add Security

--- src/main/java/edu/cmu/iccb/Application.java
+++ src/main/java/edu/cmu/iccb/Application.java
@@ -1,23 +1,76 @@
+//Reference: http://www.learningthegoodstuff.com/2014/12/spring-security-pre-authentication-and.html
+//Reference: https://github.com/FutureProcessing/spring-boot-security-example/blob/master/src/main/java/com/futureprocessing/spring/infrastructure/security/SecurityConfig.java
 package edu.cmu.iccb;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+	
 import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.*;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.security.oauth2.client.*;
+import org.springframework.security.oauth2.client.filter.*;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @SpringBootApplication
-public class Application {
+@EnableOAuth2Client
+public class Application extends WebSecurityConfigurerAdapter{
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    //Reference: https://spring.io/guides/tutorials/spring-boot-oauth2/#_social_login_manual
+    @Autowired
+    OAuth2ClientContext oauth2ClientContext;
+
+    private OAuth2ClientAuthenticationProcessingFilter ssoFilter() {
+
+        OAuth2ClientAuthenticationProcessingFilter githubFilter = new OAuth2ClientAuthenticationProcessingFilter("/login");
+        OAuth2RestTemplate githubTemplate = new OAuth2RestTemplate(github(), oauth2ClientContext);
+        githubFilter.setRestTemplate(githubTemplate);
+        githubFilter.setTokenServices(new UserInfoTokenServices(githubResource().getUserInfoUri(), github().getClientId()));
+
+        return githubFilter;
+    }
+
     @Bean
-    CommandLineRunner init() {
-		return new CommandLineRunner() {
-			@Override
-			public void run(String... args) throws Exception {}
-	    };
+    @ConfigurationProperties("github.client")
+    public AuthorizationCodeResourceDetails github() {
+        return new AuthorizationCodeResourceDetails();
+    }
+
+    @Bean
+    @ConfigurationProperties("github.resource")
+    public ResourceServerProperties githubResource() {
+        return new ResourceServerProperties();
+    }
+
+    @Bean
+    public FilterRegistrationBean oauth2ClientFilterRegistration(
+            OAuth2ClientContextFilter filter) {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(filter);
+        registration.setOrder(-100);
+        return registration;
+    }
+    
+    //Reference: http://stackoverflow.com/questions/35091817/how-to-disable-defaultsecurityfilterchain-in-a-spring-boot-app
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                    .antMatchers("/","/login/**", "/css/**", "/js/**", "/fonts/**", "/loginsuccess/**")
+                    .permitAll()
+                .anyRequest()
+                    .authenticated();
+        http.csrf().disable();
     }
 }
-
--- src/main/java/edu/cmu/iccb/HomeController.java
+++ src/main/java/edu/cmu/iccb/HomeController.java
@@ -2,16 +2,22 @@ package edu.cmu.iccb;
 
 import java.util.List;
 
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import edu.cmu.iccb.services.ImageService;
+
 
 @Controller
 public class HomeController {
@@ -36,18 +42,6 @@ public class HomeController {
         model.addAttribute("files", imageIds);     
         return "uploadForm";
     }
-
-    @RequestMapping(method = RequestMethod.GET, value = "/gitauthen")
-    public String provideUploadInfoAuthen(Model model, RedirectAttributes redirectAttributes) {
-    
-        return "redirect:https://github.com/login/oauth/authorize?client_id=ac6eaffd65bc86de1cb7";
-    }
-
-    @RequestMapping(method = RequestMethod.GET, value = "/gitauthenSuccess")
-    public String provideUploadInfoAuthenSuccess(Model model, RedirectAttributes redirectAttributes) {
-    
-        return "redirect:/images";
-    }
     
     @RequestMapping(method = RequestMethod.POST, value = "/images")
     public String handleFileUpload(@RequestParam("file") MultipartFile file,
@@ -65,6 +59,19 @@ public class HomeController {
         return "redirect:/images";
     }
 
+    @RequestMapping(method = RequestMethod.GET, value = "/loginsuccess")
+    public String githubLoginSuccess(RedirectAttributes redirectAttributes,
+                                     @CookieValue(value = "JSESSIONID") String accessToken) {
+        PreAuthenticatedAuthenticationToken auth =
+                new PreAuthenticatedAuthenticationToken("github", accessToken, Arrays.asList(new SimpleGrantedAuthority("ROLE_USER")));
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        return "redirect:/images";
+    }
+    
+    @RequestMapping("/unauthenticated")
+    public String unauthenticated() {
+        return "redirect:/error";
+    }
     
     @RequestMapping(method = RequestMethod.GET, value = "/")
     public String loginForm(Model model, RedirectAttributes redirectAttributes) {   
